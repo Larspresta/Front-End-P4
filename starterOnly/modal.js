536b3b720e81d26f463707e7c8433911fd80131f
LarsPresta <lauri.monni@gmail.com>
2023-11-24 12:10:55 +0100

    validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -16,6 +16,7 @@ const subConfirmation = document.createElement('p')
 const btnSubmit = document.getElementById('btn-sub');
 const closeForm = document.getElementById('close-form');
 
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -31,11 +32,45 @@ closeForm.addEventListener('click', () => {
 });
 
 // add confirmation element
-subConfirmation.textContent = 'Thank you for registering!';
+/* subConfirmation.textContent = 'Thank you for registering!';
 subConfirmation.style.color = '#fe142f'
 
 btnSubmit.addEventListener('click', () => {
   heroContent.appendChild(subConfirmation);
-});
+}); */
 
+const lastNameInput = document.getElementById ('last');
+const emailInput = document.getElementById ('email');
+const checkbox1Input = document.getElementById ('checkbox1');
+const firstNameInput = document.getElementById('first');
 
+function validate() {
+
+  console.log('Validation function called');
+
+    const firstNameValue = firstNameInput.value.trim();
+    const lastNameValue = lastNameInput.value.trim();
+    const emailValue = emailInput.value.trim();
+    const checkbox1Agree = checkbox1Input.checked;
+  
+
+  if (firstNameValue.length <2 || firstNameValue.length > 20) {
+    return false;
+  }
+
+  if (lastNameValue.length <2 || lastNameValue.length > 20) {
+    return false;
+  }
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(emailValue)) {
+    return false;
+  }
+
+  if (!checkbox1Agree) {
+    return false;
+  }
+
+  return true;
+
+}
